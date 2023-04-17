c659da7e3b08d9d64fd88886e6dd85125b4b9914
mohammadmmoniri <mmmoniri77@gmail.com>
2023-04-18 01:43:48 +0330

    add database and config module to project

--- assets/src/app.module.ts
+++ assets/src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+
+import { WalletModule } from './wallet-module/wallet.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            load: [configuration],
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('database.host'),
+                port: +configService.get('database.port'),
+                username: configService.get('database.username'),
+                password: configService.get('database.password'),
+                database: configService.get('database.database'),
+                entities: [],
+                synchronize: true,
+            }),
+            inject: [ConfigService],
+        }),
+        WalletModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
